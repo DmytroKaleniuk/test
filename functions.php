f6e78f6b8038fa1d51840c66f88cd07fb2b1077c
DmytroKaleniuk <kaleniuk1989@gmail.com>
2016-07-17 23:28:23 +0300

    new changes
    changes 7/17/2016

--- functions.php
+++ functions.php
@@ -7,3 +7,14 @@ function test_script_enqueue() {
 }
 
 add_action( 'wp_enqueue_scripts', 'test_script_enqueue');
+
+
+function test_theme_setup() {
+		
+	add_theme_support('menus');
+	register_nav_menu('primary', 'Primary Header Navigation'); 
+    register_nav_menu('secondary', 'Footer Navigation'); 
+
+}
+
+add_action('init', 'test_theme_setup');
